55e5d60f1f135115b24b7d84d0ca7aedff580658
Ari Gesher <ari@gesher.net>
2016-02-16 16:14:00 -0800

    First attempt at packaging new version of the ESRI plugin

--- package.js
+++ package.js
@@ -1,7 +1,11 @@
 Package.describe({
-  summary: "Esri Leaflet supports loading Esri basemaps and feature services, as well as tiled and dynamic map services."
+  summary: "See https://github.com/Esri/esri-leaflet/tree/v2.0.0-beta.7",
+  version: '2.0.0b7',
+  name: 'kairos:esri-leaflet',
+  git: 'https://github.com/KairosAerospace/esri-leaflet'
 });
 
 Package.on_use(function (api) {
-  api.add_files(['esri-leaflet.js', 'leaflet.js'], 'client');
+  api.use('bevanhunt:leaflet', 'client');
+  api.add_files(['esri-leaflet.js'], 'client');
 });
